2212b1bae77c7e508cac0449d07d40833912611e
Andrew Tu <drewtu2@yahoo.com>
2019-01-31 13:17:07 -0500

    added c++11 support for use with chrono

--- hw2/q2/src/primes.cpp
+++ hw2/q2/src/primes.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
+
 #include "FindPrimes.h"
 
 using std::vector;
 using std::cout;
 using std::cin;
 using std::endl;
+
+typedef std::chrono::high_resolution_clock Clock;
 
 int main() {
     int num_threads;
@@ -16,14 +20,20 @@ int main() {
     cout << "\nUp to? : " <<endl;
     cin >> max_num;
 
+    auto t1 = Clock::now();
     FindPrimes finder(num_threads);
-    
     vector<int> primes = finder.primes_to_n(max_num);
+    auto t2 = Clock::now();
+
 
     cout << primes.size() << " total primes between 1 and " << max_num << endl;
-    for(vector<int>::iterator it = primes.begin(); it != primes.end(); it++) {
-        cout << *it << endl;
-    }
+    //for(vector<int>::iterator it = primes.begin(); it != primes.end(); it++) {
+    //    cout << *it << endl;
+    //}
+
+    cout << "Time to find: " 
+        << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
+        << " ms" << std::endl;
 
     return 0;
 }
