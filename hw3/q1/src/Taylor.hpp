adf6663b240317ff3910aaf8a011375d2f801bb4
Andrew Tu <drewtu2@yahoo.com>
2019-02-18 13:26:29 -0500

    added q1 base code

---
+++ hw3/q1/src/Taylor.hpp
@@ -0,0 +1,56 @@
+#ifndef __TAYLOR
+#define __TAYLOR
+
+template<class T> class Taylor {
+public:
+ T run(T input, int terms);
+ T pow(T base, int exp);
+ int factorial(int N);
+
+};
+
+template<class T> T Taylor<T>::run(T input, int terms) {
+  T result = input;
+  int coeff;
+  int term;
+
+  for(int ii = 1; ii < terms; ++ii) {
+    if(ii%2 == 0) {
+      coeff = 1; // Even term, want to add this term
+    } else {
+      coeff = -1; // Odd term, want to subtract this term
+    }
+
+    term = (2*ii) + 1;
+
+    result += (coeff * pow(input, term)/factorial(term));
+    
+  }
+
+  return result;
+}
+
+template<class T> T Taylor<T>::pow(T base, int exp) {
+
+  T result = 1;
+
+  for(int ii = 0; ii < exp; ++ii) {
+    result *= base;
+  }
+
+  return result;
+}
+
+template<class T> int Taylor<T>::factorial(int N) {
+
+  int result = 1;
+  
+  for(int ii = 2; ii <= N; ++ii) {
+    result *= ii;
+  }
+
+  return result;
+}
+
+
+#endif
---
+++ hw3/q1/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Taylor.hpp"
+
+template<class T> inline T f(T input, int num_terms) {
+
+  Taylor<T> taylor_t;
+
+  return input - taylor_t.run(input, num_terms);
+
+}
+
+int main() 
+{
+  double input_d = 1.0;
+  float input_f = 1.0;
+  int num_terms = 15;
+
+  std::cout << "float: " << f(input_f, num_terms) << std::endl;
+  std::cout << "double: " << f(input_d, num_terms) << std::endl;
+
+  input_d = 2300.0;
+  input_f = 2300.0;
+  
+  std::cout << "float: " << f(input_f, num_terms) << std::endl;
+  std::cout << "double: " << f(input_d, num_terms) << std::endl;
+ 
+  input_d = -.45;
+  input_f = -.45;
+  
+  std::cout << "float: " << f(input_f, num_terms) << std::endl;
+  std::cout << "double: " << f(input_d, num_terms) << std::endl;
+
+
+  return 0;
+}
