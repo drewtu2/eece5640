52047640402e82689949d1a2e3d4b1baf53989c0
Andrew Tu <drewtu2@yahoo.com>
2019-03-03 22:03:05 -0500

    matrix matrix

---
+++ hw4/q2/matrix-matrix.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+#define M 256 
+#define B 64
+
+double CLOCK() {
+        struct timespec t;
+        clock_gettime(CLOCK_MONOTONIC,  &t);
+        return (t.tv_sec * 1000)+(t.tv_nsec*1e-6);
+}
+
+main(int argc, char **argv)
+{
+    int i,j,k,jj,kk,en;
+    double start, finish, total, sum;
+    int a[M][M], b[M][M], c[M][M];
+
+    for (i=0; i<M; i++)
+       for (j=0; j<M; j++)
+          a[i][j] = i+j;
+
+    for (i=0; i<M; i++)
+       for (j=0; j<M; j++)
+           b[i][j] = i+j;
+
+    for (i=0; i<M; i++)
+       for (j=0; j<M; j++)
+           c[i][j] = 0;
+
+    en = B * 4;
+    start = CLOCK();
+
+    for (kk=0; kk<en; kk+=B)
+      for (jj=0; jj<en; jj+=B)
+         for (i=0; i< M; i++)
+            for (j = jj; j< jj + B; j++) {
+               sum = c[i][j];
+               for (k=kk; k< kk + B; k++) {
+                  sum+= a[i][j] * b[k][j];
+                  }
+                 c[i][j] = sum;
+               }
+
+    finish = CLOCK();
+    total = finish - start;
+    printf("Time for the loop = %f\n", total);
+
+    return 0;
+}
