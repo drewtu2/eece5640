993397626618ecf90420ad0742eedc88301ed366
Andrew Tu <drewtu2@yahoo.com>
2019-04-08 13:39:17 -0400

    updated some harris work

---
+++ final_project/harris/include/HarrisCorner.h
@@ -0,0 +1,70 @@
+#ifndef __HARRIS_CORNER_H__
+#define __HARRIS_CORNER_H__
+
+#include <opencv2/features2d.hpp>
+
+class HarrisCorner {
+
+ private:
+  float k;
+  float corner_response_threshold;
+
+ public:
+
+  /**
+   * Factor method to create a Harris corner detector object
+   */
+  static HarrisCorner* create(float k, float threshold);
+  
+  /**
+   * Creates an OpenMP accelerated Harris Corner Detector
+   */
+  static HarrisCorner* createOpenMP(float k, float threshold);
+
+  /**
+   * Default constructor for Harris Corner Detector
+   */
+  HarrisCorner HarrisCorner();
+
+  /**
+   * Choose different k constructor for Harris Corner Detector
+   */
+  HarrisCorner HarrisCorner(float k, float threshold);
+
+
+  /**
+   * Populates a vector with a list of corner keypoints detected in the given
+   * image. Mask can dictate what parts of the image to use. 
+   */
+  void detect(InputArray image, std::vector< KeyPoint > &keypoints, InputArray mask=noArray());
+
+  /**
+   * Detects Harris Corners across multiple images.
+   *
+   */
+  void detect(InputArrayOfArrays images, std::vector<std::vector< KeyPoint > > &keypoints, InputArrayOfArrays masks=noArray());
+
+
+  /**
+   * Computes Harris Corner Images on a single image 
+   * 
+   * @param image: the image to detect corners on
+   * @param keypoints: the keypoints where the points were found
+   * @param descriptors: an array of descriptors for each keypoint
+   */
+  void compute(InputArray image, std::vector<KeyPoint> &keypoints, OutputArray descriptors);
+
+  /**
+   * Computes Harris Corner Images on a series of images
+   * 
+   * @param image: the images to detect corners on
+   * @param keypoints: the keypoints where the points were found a vector for
+   * each image
+   * @param descriptors: an array of descriptors for each keypoint. an array for
+   * each image
+   */
+  void compute(InputArrayofArrays images, std::vector<std::vector<KeyPoint>> &keypoints, OutputArrayOfArrays descriptors);
+
+}
+
+#endif
---
+++ final_project/harris/src/harris.cpp
@@ -0,0 +1,73 @@
+#include "HarrisCorner.h"
+
+HarrisCorner* HarrisCorner::create(float k, float threshold) {
+	return new HarrisCorner(k, threshold);
+}
+
+HarrisCorner* HarrisCorner::createOpenMP(float k, float threshold) {
+	//TODO: Update this to use an openmp version
+	return new HarrisCorner(k, threshold);
+}
+
+HarrisCorner::HarrisCorner() {
+	this->k = .04f;
+	this->corner_response_threshold = 1e5f;
+}
+
+HarrisCorner::HarrisCorner(float k, float threshold) {
+	this->k = k;
+	this->corner_response_threshold = threshold;
+}
+
+void detect(InputArray image, std::vector<KeyPoint> &keypoints, InputArray mask=noArray()) {
+	// Used the following code as reference
+	// http://arrayfire.org/docs/computer_vision_2harris_8cpp-example.htm
+	Mat ix = Mat::zeros(image.rows, image.cols)
+			Mat iy = Mat::zeros(image.rows, image.cols)
+
+			this->calculate_gradients(ix, iy, image);
+
+	// Multiply matrices together
+	Mat ixx = ix.mul(ix);
+	Mat iyy = iy.mul(iy);
+	Mat ixy = iy.mul(ix);
+
+	// Harris Corner Response Matrix: 
+	// [ixx, ixy;
+	// ixy, iyy]
+
+	// Trace is added together the main diagonal
+	Mat itrace = ixx + iyy;
+
+	// Determinant at each pixel is the difference of the two diagonals
+	Mat idet = (ixx * iyy) - (ixy * ixy);
+
+	// Response
+	Mat response = idet - this->f * (itrace * itrace);
+
+
+	// Extract keyponts from image
+	int channels = response.channels();
+	int nRows = response.rows;
+	int nCols = response.cols * channels;
+
+	if (response.isContinuous())
+	{
+		nCols *= nRows;
+		nRows = 1;
+	}
+
+	uchar* p;
+	for(int i = 0; i < nRows; ++i) {
+		// get the row ptr
+		p = response.ptr<uchar>(i);
+		for(int j = 0; j < nCols; ++j)
+		{
+			if (p[j] > this.corner_response_threshold) {
+				keypoints.push_back(KeyPoint(i, j));
+			}
+		}
+	}
+
+	return I;
+}
